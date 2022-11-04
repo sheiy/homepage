144ab8ea47fee8b8cd1953bcb23254d7d05b34f3
sheiy <xlz35429674@foxmail.com>
2022-11-04 09:47:05 +0800

    init

--- src/main/java/site/ownw/homepage/domain/system/ConfigService.java
+++ src/main/java/site/ownw/homepage/domain/system/ConfigService.java
@@ -1,10 +1,11 @@
 package site.ownw.homepage.domain.system;
 
 import java.util.Map;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import site.ownw.homepage.common.exception.EntityNotFoundException;
 import site.ownw.homepage.domain.system.repository.SystemConfigRepository;
+import site.ownw.homepage.entity.SystemConfig;
 
 @Service
 @RequiredArgsConstructor
@@ -13,9 +14,11 @@ public class ConfigService {
     private final SystemConfigRepository systemConfigRepository;
 
     public Map<String, Object> getConfig(String key) {
-        return systemConfigRepository
-                .findByKey(key)
-                .orElseThrow(() -> new EntityNotFoundException("SystemConfig", key))
-                .getMapContent();
+        Optional<SystemConfig> systemConfig = systemConfigRepository.findByKey(key);
+        if (systemConfig.isPresent()) {
+            return systemConfig.get().getMapContent();
+        } else {
+            return Map.of();
+        }
     }
 }
